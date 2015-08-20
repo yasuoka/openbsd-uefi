08f6724b95fb8454a198f6f9a4774c7d73e757a6
YASUOKA Masahiko <yasuoka@yasuoka.net>
2015-08-20 21:22:39 +0900

    Use 80x31 console as the maximum.  Because large size of console is just using CPU.  Seriously it makes boot slow.  Also some refactors are done.

--- src/sys/arch/amd64/amd64/efifb.c
+++ src/sys/arch/amd64/amd64/efifb.c
@@ -20,6 +20,7 @@
 #include <sys/systm.h>
 
 #include <uvm/uvm_extern.h>
+#include <machine/bus.h>
 
 #include <dev/wscons/wsconsio.h>
 #include <dev/wscons/wsdisplayvar.h>
@@ -27,8 +28,21 @@
 
 #include <machine/biosvar.h>
 
+struct efifb {
+	struct rasops_info	 rinfo;
+	int			 depth;
+	paddr_t			 paddr;
+	psize_t			 psize;
+};
+
+struct efifb_softc {
+	struct device	 sc_dev;
+	struct efifb	*sc_fb;
+};
+
 int	 efifb_match(struct device *, void *, void *);
 void	 efifb_attach(struct device *, struct device *, void *);
+void	 efifb_rasops_preinit(struct efifb *);
 int	 efifb_ioctl(void *, u_long, caddr_t, int, struct proc *);
 paddr_t	 efifb_mmap(void *, off_t, int);
 int	 efifb_alloc_screen(void *, const struct wsscreen_descr *, void **,
@@ -39,20 +53,14 @@ int	 efifb_show_screen(void *, void *, int, void (*cb) (void *, int, int),
 int	 efifb_list_font(void *, struct wsdisplay_font *);
 int	 efifb_load_font(void *, void *, struct wsdisplay_font *);
 
-struct efifb {
-	struct rasops_info	 rinfo;
-};
-
-struct efifb_softc {
-	struct device	 sc_dev;
-	struct efifb	*sc_fb;
-};
-
 struct cfattach efifb_ca = {
 	sizeof(struct efifb_softc), efifb_match, efifb_attach, NULL
 };
 
-struct wsscreen_descr efifb_std_descr = { "efi" };
+#define	EFIFB_WIDTH	100
+#define	EFIFB_HEIGHT	31
+
+struct wsscreen_descr efifb_std_descr = { "std" };
 
 const struct wsscreen_descr *efifb_descrs[] = {
 	&efifb_std_descr
@@ -92,34 +100,59 @@ efifb_match(struct device *parent, void *cf, void *aux)
 void
 efifb_attach(struct device *parent, struct device *self, void *aux)
 {
+	struct efifb		*fb;
 	struct efifb_softc	*sc = (struct efifb_softc *)self;
 	struct wsemuldisplaydev_attach_args
 				 aa;
 	struct rasops_info 	*ri;
-	int			 ccol, crow;
+	int			 ccol = 0, crow = 0;
 	long			 defattr;
 
 	printf("\n");
-	if (1) {
-		aa.console = 1;	/* XXX */
-		sc->sc_fb = &efifb_console;
-		ri = &sc->sc_fb->rinfo;
+
+	if (1) {	/* XXX console */
+		aa.console = 1;
+		fb = sc->sc_fb = &efifb_console;
+		ri = &fb->rinfo;
+		ccol = ri->ri_ccol;
+		crow = ri->ri_crow;
+
+		efifb_rasops_preinit(fb);
+		ri->ri_flg &= ~RI_CLEAR;
+		ri->ri_flg |= RI_VCONS;
+
+		rasops_init(ri, efifb_std_descr.nrows, efifb_std_descr.ncols);
 	}
+
 	aa.scrdata = &efifb_screen_list;
 	aa.accessops = &efifb_accessops;
 	aa.accesscookie = sc;
 	aa.defaultscreens = 0;
 
-	ccol = ri->ri_ccol;
-	crow = ri->ri_crow;
-
-	ri->ri_flg = RI_VCONS;
-	rasops_init(ri, 160, 160);
-
 	ri->ri_ops.alloc_attr(ri->ri_active, 0, 0, 0, &defattr);
 	wsdisplay_cnattach(&efifb_std_descr, ri->ri_active, ccol, crow, defattr);
 
 	config_found(self, &aa, wsemuldisplaydevprint);
+}
+
+void
+efifb_rasops_preinit(struct efifb *fb)
+{
+#define bmnum(_x) (fls(_x) - ffs(_x) + 1)
+#define bmpos(_x) (ffs(_x) - 1)
+	extern bios_efiinfo_t	*bios_efiinfo;
+	struct rasops_info	*ri = &fb->rinfo;
+
+	ri->ri_width = bios_efiinfo->fb_width;
+	ri->ri_height = bios_efiinfo->fb_height;
+	ri->ri_depth = fb->depth;
+	ri->ri_stride = bios_efiinfo->fb_pixpsl * (fb->depth / 8);
+	ri->ri_rnum = bmnum(bios_efiinfo->fb_red_mask);
+	ri->ri_rpos = bmpos(bios_efiinfo->fb_red_mask);
+	ri->ri_gnum = bmnum(bios_efiinfo->fb_green_mask);
+	ri->ri_gpos = bmpos(bios_efiinfo->fb_green_mask);
+	ri->ri_bnum = bmnum(bios_efiinfo->fb_blue_mask);
+	ri->ri_bpos = bmpos(bios_efiinfo->fb_blue_mask);
 }
 
 int
@@ -174,12 +207,11 @@ paddr_t
 efifb_mmap(void *v, off_t off, int prot)
 {
 	struct efifb_softc	*sc = v;
-	struct rasops_info	*ri = &sc->sc_fb->rinfo;
 
-	if (off > ri->ri_height * ri->ri_stride)
+	if (off > sc->sc_fb->psize)
 		return (-1);
 
-	return (PMAP_DIRECT_UNMAP(ri->ri_bits) + off);
+	return (sc->sc_fb->paddr + off);
 }
 
 int
@@ -229,9 +261,6 @@ efifb_list_font(void *v, struct wsdisplay_font *font)
 	return (rasops_list_font(ri, font));
 }
 
-#define bmnum(_x) (fls(_x) - ffs(_x) + 1)
-#define bmpos(_x) (ffs(_x) - 1)
-
 int
 efifb_cnattach(void)
 {
@@ -239,36 +268,27 @@ efifb_cnattach(void)
 	struct efifb		*fb = &efifb_console;
 	struct rasops_info	*ri = &fb->rinfo;
 	long			 defattr = 0;
-	int			 depth;
 
 	if (bios_efiinfo == NULL || bios_efiinfo->fb_addr == 0)
 		return (-1);
 
 	memset(&efifb_console, 0, sizeof(efifb_console));
 
-	ri->ri_bits = (u_char *)PMAP_DIRECT_MAP(bios_efiinfo->fb_addr);
+	fb = &efifb_console;
+	fb->paddr = bios_efiinfo->fb_addr;
+	fb->depth = max(fb->depth, fls(bios_efiinfo->fb_red_mask));
+	fb->depth = max(fb->depth, fls(bios_efiinfo->fb_green_mask));
+	fb->depth = max(fb->depth, fls(bios_efiinfo->fb_blue_mask));
+	fb->depth = max(fb->depth, fls(bios_efiinfo->fb_reserved_mask));
+	fb->psize = bios_efiinfo->fb_height *
+	    bios_efiinfo->fb_pixpsl * (fb->depth / 8);
 
-	ri->ri_width = bios_efiinfo->fb_width;
-	ri->ri_height = bios_efiinfo->fb_height;
+	ri->ri_bits = (u_char *)PMAP_DIRECT_MAP(fb->paddr);
 
-	depth = 0;
-	depth = max(depth, fls(bios_efiinfo->fb_red_mask));
-	depth = max(depth, fls(bios_efiinfo->fb_green_mask));
-	depth = max(depth, fls(bios_efiinfo->fb_blue_mask));
-	depth = max(depth, fls(bios_efiinfo->fb_reserved_mask));
-	ri->ri_depth = depth;
-	ri->ri_stride = bios_efiinfo->fb_pixpsl * (ri->ri_depth / 8);
+	efifb_rasops_preinit(fb);
 
-	ri->ri_rnum = bmnum(bios_efiinfo->fb_red_mask);
-	ri->ri_rpos = bmpos(bios_efiinfo->fb_red_mask);
-	ri->ri_gnum = bmnum(bios_efiinfo->fb_green_mask);
-	ri->ri_gpos = bmpos(bios_efiinfo->fb_green_mask);
-	ri->ri_bnum = bmnum(bios_efiinfo->fb_blue_mask);
-	ri->ri_bpos = bmpos(bios_efiinfo->fb_blue_mask);
-	ri->ri_flg = RI_CLEAR;
-
-	rasops_init(ri, 1600, 1600);
-
+	ri->ri_flg = RI_CLEAR | RI_CENTER;
+	rasops_init(ri, EFIFB_HEIGHT, EFIFB_WIDTH);
 	efifb_std_descr.ncols = ri->ri_cols;
 	efifb_std_descr.nrows = ri->ri_rows;
 	efifb_std_descr.textops = &ri->ri_ops;
@@ -277,7 +297,6 @@ efifb_cnattach(void)
 	efifb_std_descr.capabilities = ri->ri_caps;
 
 	ri->ri_ops.alloc_attr(ri, 0, 0, 0, &defattr);
-
 	wsdisplay_cnattach(&efifb_std_descr, ri, 0, 0, defattr);
 
 	return (0);
